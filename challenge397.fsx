69c8f5cedc701c362f1bd38a614fb7ec17cbd843
codehound42 <41467365+codehound42@users.noreply.github.com>
2022-09-25 19:04:42 +0200

    Finish challenge 397 (basic)
    Currently quite a lot of code to do a simple thing, but havent invested more time in refactoring

--- challenge397.fsx
+++ challenge397.fsx
@@ -1,6 +1,18 @@
-// TODO: Finish up
+// Challenge397
+// Note: Returned value format differs from original problem formulation
+// Comparison implemented as operator instead of true/false result
 
-let numeralToValue = Map.ofList [('I', 1); ('V', 5); ('X', 10); ('L', 50); ('C', 100); ('D', 500); ('M', 1000)]
+let numeralToValue = Map.ofList [('I', 1); ('V', 5); ('X', 10); ('L', 50); ('C', 100); ('D', 500); ('M', 1000); ('-', 0)]
+
+let combine xs ys = 
+    let xsLength = List.length xs
+    let ysLength = List.length ys
+    if (xsLength > ysLength) then
+        (xs, List.append ys [for _ in 1 .. (xsLength - ysLength) -> '-'])
+    elif (xsLength < ysLength) then
+        (List.append xs [for _ in 1 .. (ysLength - xsLength) -> '-'], ys)
+    else
+        (xs, ys)
 
 let rec numcompareAux combined =
     match combined with
@@ -18,13 +30,19 @@ let rec numcompareAux combined =
                 numcompareAux tail
         with
             | :? System.Collections.Generic.KeyNotFoundException -> failwith "Invalid char value"
-    
 
 let numcompare (input1: string) (input2: string) =
     let xs = Seq.toList input1
     let ys = Seq.toList input2
-    let combined = List.zip xs ys
-    let result = numcompareAux combined
-    printfn "%i" result
+    let combined = combine xs ys
+    
+    let result = List.zip (fst combined) (snd combined) |> numcompareAux
+    printfn "%A" result
+    result
 
-numcompare "V" "I"
+numcompare "I" "I"  // false
+numcompare "I" "II"  // true
+numcompare "II" "I"  // false
+numcompare "V" "IIII"  // false
+numcompare "MDCLXV" "MDCLXVI"  // true
+numcompare "MM" "MDCCCCLXXXXVIIII"  // false
